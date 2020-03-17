ae24f5e46270fb2e15da1fb481840510e9aacfad
Nick Howes <n.howes@warwick.ac.uk>
2020-03-17 19:28:42 +0000

    OE-25 Fix log mistake and fallback to console.log if console.warn/error not available.

--- app/assets/js/log.js
+++ app/assets/js/log.js
@@ -3,9 +3,9 @@
 const noop = () => {};
 
 /* eslint-disable no-console */
-const info = window.console ? console.log : noop;
-const error = (window.console && console.error) ? console.error : noop;
-const warn = (window.console && console.error) ? console.error : noop;
+const info = (window.console && console.info) ? console.log : noop;
+const error = (window.console && console.error) ? console.error : info;
+const warn = (window.console && console.warn) ? console.error : info;
 
 export { error, warn, info };
 export default info;
