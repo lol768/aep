d9cf9acfb31afca4efae578f6b532876ce3a40b9
u1574999 <f.perez-diaz@warwick.ac.uk>
2020-03-24 10:33:25 +0000

    OE-31 Renaming of method

--- app/domain/StudentAssessmentWithAssessment.scala
+++ app/domain/StudentAssessmentWithAssessment.scala
@@ -1,11 +1,9 @@
 package domain
 
-import warwick.core.helpers.JavaTime
-
 case class StudentAssessmentWithAssessment(
   studentAssessment: StudentAssessment,
   assessment: Assessment
 ) {
   // TODO Add logic for when exam is finalised
-  def isCurrentlyDoingExam = studentAssessment.startTime.nonEmpty
+  def inProgress = studentAssessment.startTime.nonEmpty
 }
