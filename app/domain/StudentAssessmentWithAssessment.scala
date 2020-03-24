5fbceec0ef1071b80ba28da23223818e10f7332e
u1574999 <f.perez-diaz@warwick.ac.uk>
2020-03-24 09:45:21 +0000

    OE-31 Open websocket when exam is taking place

--- app/domain/StudentAssessmentWithAssessment.scala
+++ app/domain/StudentAssessmentWithAssessment.scala
@@ -1,6 +1,10 @@
 package domain
 
+import warwick.core.helpers.JavaTime
+
 case class StudentAssessmentWithAssessment(
   studentAssessment: StudentAssessment,
   assessment: Assessment
-)
+) {
+  def isCurrentlyDoingExam = studentAssessment.startTime.exists(_.plus(assessment.duration).isBefore(JavaTime.offsetDateTime))
+}
