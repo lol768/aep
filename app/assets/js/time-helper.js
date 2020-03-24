929e3954c3cb39a20df58ce0b24dc51993f73d96
Kai Lan <k.lan@warwick.ac.uk>
2020-03-24 11:37:51 +0000

    OE-50 extract countdown wip

--- app/assets/js/time-helper.js
+++ app/assets/js/time-helper.js
@@ -1,23 +1,4 @@
 export const msToHumanReadable = (duration) => {
-  // let seconds = Math.floor((duration / 1000) % 60);
-  // let minutes = Math.floor((duration / (1000 * 60)) % 60);
-  // let hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
-  // let days = Math.floor((duration / (1000 * 60 * 60 * 24)) % 24);
-  //
-  // // minutes = (hours % 2 === 0) ? `${minutes} minutes` : `${minutes} minute`;
-  // // hours = (hours % 2 === 0) ? `${hours} hours` : `${hours} hour`;
-  // // days = (days % 2 === 0) ? `${days} days` : `${days} day`;
-  //
-  // // const plural = (quantity, unit) => {
-  // //   (quantity % 2 === 0) ? `${quantity} ${unit}s` : `${hours} ${unit}`;
-  // // };
-  // //
-  // // const data = {
-  // //   minute: Math.floor((duration / (1000 * 60)) % 60),
-  // //   hour: Math.floor((duration / (1000 * 60 * 60)) % 24),
-  // //   day: Math.floor((duration / (1000 * 60 * 60 * 24)) % 24),
-  // // }
-
   return Object.entries({
     day: Math.floor((duration / (1000 * 60 * 60 * 24)) % 24),
     hour: Math.floor((duration / (1000 * 60 * 60)) % 24),
