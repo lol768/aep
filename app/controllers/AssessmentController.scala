369134530faa81cc59717e3fc5280e6ff1d34854
Mat Mannion <M.Mannion@warwick.ac.uk>
2020-03-24 09:47:03 +0000

    OE-53 Controller action for downloading files from the assessment brief

--- app/controllers/AssessmentController.scala
+++ app/controllers/AssessmentController.scala
@@ -2,18 +2,19 @@ package controllers
 
 import java.util.UUID
 
-import domain.StudentAssessmentWithAssessment
 import javax.inject.{Inject, Singleton}
-import play.api.mvc.{Action, AnyContent}
-import services.{AssessmentService, SecurityService, StudentAssessmentService}
+import play.api.mvc.{Action, AnyContent, Result}
+import services.{SecurityService, StudentAssessmentService}
+import warwick.fileuploads.UploadedFileControllerHelper
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
 class AssessmentController @Inject()(
   security: SecurityService,
   studentAssessmentService: StudentAssessmentService,
-)( implicit
+  uploadedFileControllerHelper: UploadedFileControllerHelper,
+)(implicit
   ec: ExecutionContext,
 ) extends BaseController {
   import security._
@@ -23,8 +24,17 @@ class AssessmentController @Inject()(
   }
 
   def start(assessmentId: UUID): Action[AnyContent] = StudentAssessmentAction(assessmentId).async { implicit request =>
-    studentAssessmentService.startAssessment(request.studentAssessmentWithAssessment.studentAssessment).successMap { studentAssessment =>
+    studentAssessmentService.startAssessment(request.studentAssessmentWithAssessment.studentAssessment).successMap { _ =>
       Redirect(controllers.routes.AssessmentController.view(assessmentId))
     }
+  }
+
+  def downloadFile(assessmentId: UUID, fileId: UUID): Action[AnyContent] = StudentAssessmentIsStartedAction(assessmentId).async { implicit request =>
+    def notFound: Future[Result] =
+      Future.successful(NotFound(views.html.errors.notFound()))
+
+    request.studentAssessmentWithAssessment.assessment.brief.files
+      .find(_.id == fileId)
+      .fold(notFound)(uploadedFileControllerHelper.serveFile)
   }
 }
