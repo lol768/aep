6ff370d1c9cb9318d0093366c548094c031c4ceb
Kai Lan <k.lan@warwick.ac.uk>
2020-03-24 12:22:01 +0000

    OE-50 add `,` and `and`

--- app/assets/js/time-helper.js
+++ app/assets/js/time-helper.js
@@ -1,10 +1,20 @@
 export default function msToHumanReadable(duration) {
-  return Object.entries({
+  const arrayToSentence = arr => {
+    if (!arr) return;
+    const {length} = arr;
+    if (length === 0) return;
+    if (length === 1) return arr.pop();
+    if (length === 2) return arr.join(' and ');
+    const last = arr.pop();
+    return `${arr.join(', ')} and ${last}`;
+  };
+
+  return arrayToSentence(Object.entries({
     day: Math.floor((duration / (1000 * 60 * 60 * 24)) % 24),
     hour: Math.floor((duration / (1000 * 60 * 60)) % 24),
     minute: Math.floor((duration / (1000 * 60)) % 60),
   }).map(([unit, quantity]) => {
     if (quantity === 0) return null;
     return (quantity % 2 === 0) ? `${quantity} ${unit}s` : `${quantity} ${unit}`;
-  }).filter(Boolean).join(' ');
+  }).filter(Boolean));
 }
--- test/assets/js/time-helper-test.js
+++ test/assets/js/time-helper-test.js
@@ -1,15 +1,15 @@
-import {msToHumanReadable} from "../../../app/assets/js/time-helper";
+import msToHumanReadable from "../../../app/assets/js/time-helper";
 
 describe('time-helper', () => {
   it('msToHumanReadable', () => {
     assert.equal(msToHumanReadable(60000), '1 minute');
     assert.equal(msToHumanReadable(60000 * 2), '2 minutes');
     assert.equal(msToHumanReadable(60000 * 60), '1 hour');
-    assert.equal(msToHumanReadable(60000 * 60 * 2 + 60000), '2 hours 1 minute');
-    assert.equal(msToHumanReadable(60000 * 60 * 2 + 60000 * 2), '2 hours 2 minutes');
+    assert.equal(msToHumanReadable(60000 * 60 * 2 + 60000), '2 hours and 1 minute');
+    assert.equal(msToHumanReadable(60000 * 60 * 2 + 60000 * 2), '2 hours and 2 minutes');
     assert.equal(msToHumanReadable(60000 * 60 * 24), '1 day');
     assert.equal(msToHumanReadable(60000 * 60 * 24 * 2), '2 days');
-    assert.equal(msToHumanReadable(60000 * 60 * 24 * 2 + 60000 * 60 * 2 + 60000), '2 days 2 hours 1 minute');
-    assert.equal(msToHumanReadable(60000 * 60 * 24 * 2 + 60000 * 60 + 60000), '2 days 1 hour 1 minute');
+    assert.equal(msToHumanReadable(60000 * 60 * 24 * 2 + 60000 * 60 * 2 + 60000), '2 days, 2 hours and 1 minute');
+    assert.equal(msToHumanReadable(60000 * 60 * 24 * 2 + 60000 * 60 + 60000), '2 days, 1 hour and 1 minute');
   });
 });
