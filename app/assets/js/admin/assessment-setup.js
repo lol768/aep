d6de9a8288ec28bb56ed859e227afde455c2958e
Alec Cursley <alec@cursley.net>
2020-03-26 17:03:09 +0000

    OE-67 Disable hidden field

--- app/assets/js/admin/assessment-setup.js
+++ app/assets/js/admin/assessment-setup.js
@@ -4,7 +4,9 @@ $(() => {
   $('#platform_field').find('input:radio').on('change', () => {
     const isOnlineExams = $('#platform_OnlineExams').is(':checked');
 
-    $('#url_field').toggleClass('hide', isOnlineExams);
+    $('#url_field').toggleClass('hide', isOnlineExams)
+      .find('input').prop('disabled', isOnlineExams);
+
     $('#files_field').toggleClass('hide', !isOnlineExams);
   }).trigger('change');
 });
