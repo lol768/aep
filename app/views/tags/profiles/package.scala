b7833ad91d9295ba0d5eff903696a155bb8aa55e
Mat Mannion <M.Mannion@warwick.ac.uk>
2020-03-31 13:55:08 +0100

    OE-100 Import basic profile information from Tabula; show a list of test users to masquerade as on the masquerade page

---
+++ app/views/tags/profiles/package.scala
@@ -0,0 +1,36 @@
+package views.tags
+
+import domain.tabula.{SitsProfile, UserType}
+
+package object profiles {
+
+  def typeAndDepartment(client: SitsProfile): String = client.userType match {
+    case UserType.Student =>
+      val year = client.yearOfStudy.flatMap(_.level) match {
+        case Some("1") => "First year"
+        case Some("2") => "Second year"
+        case Some("3") => "Third year"
+        case Some(level) if level.forall(_.isDigit) => s"${level}th year"
+        case Some(_) if client.attendance.nonEmpty => client.attendance.get.description
+        case _ => ""
+      }
+      (Seq(year) ++ client.group ++ Seq("student,", client.department.name)).mkString(" ")
+    case _ =>
+      s"${client.userType}, ${client.department.name}"
+  }
+
+  def typeAndAttendance(client: SitsProfile): String = client.userType match {
+    case UserType.Student =>
+      val year = client.yearOfStudy.flatMap(_.level) match {
+        case Some("1") => "First year"
+        case Some("2") => "Second year"
+        case Some("3") => "Third year"
+        case Some(level) if level.forall(_.isDigit) => s"${level}th year"
+        case _ => ""
+      }
+      (Seq(year) ++ client.group ++ Seq("student,") ++ client.attendance.map(_.description).toSeq).mkString(" ")
+    case _ =>
+      client.userType.entryName
+  }
+
+}
