c45742a431b15c2391459cdb8c2a587c7aa3fe33
Kai Lan <k.lan@warwick.ac.uk>
2020-04-01 17:02:25 +0100

    OE-45 wip

--- app/controllers/invigilation/InvigilatorAssessmentController.scala
+++ app/controllers/invigilation/InvigilatorAssessmentController.scala
@@ -6,7 +6,8 @@ import controllers.BaseController
 import javax.inject.Inject
 import play.api.mvc.{Action, AnyContent}
 import services.refiners.ActionRefiners
-import services.{AssessmentService, ReportingService, SecurityService}
+import services.{AssessmentService, ReportingService, SecurityService, StudentAssessmentService}
+import warwick.sso.{Name, UserLookupService, Usercode}
 
 import scala.concurrent.ExecutionContext
 
@@ -14,15 +15,41 @@ class InvigilatorAssessmentController @Inject()(
   security: SecurityService,
   actionRefiners: ActionRefiners,
   assessmentService: AssessmentService,
-  reportingService: ReportingService
+  reportingService: ReportingService,
+  userLookup: UserLookupService,
+  studentAssessmentService: StudentAssessmentService,
 )(implicit ec: ExecutionContext) extends BaseController {
+
   import security._
 
-  def view(assessmentId: UUID): Action[AnyContent] = InvigilatorAsseessmentAction(assessmentId) { implicit req =>
-    Ok(views.html.invigilation.assessment(req.assessment))
+  def view(assessmentId: UUID): Action[AnyContent] = InvigilatorAsseessmentAction(assessmentId).async { implicit req =>
+
+    val assessment = req.assessment
+    // inflate dept code
+
+    studentAssessmentService.byAssessmentId(assessmentId).successMap { students =>
+      Ok(views.html.invigilation.assessment(
+        assessment = assessment,
+        userNames = assessment.invigilators
+          .map(userLookup.getUser)
+          .flatMap(_.fold(
+            _ => None,
+            user => Some(user)
+          ))
+          .filter(_.isFound)
+          .filter(_.name.full.nonEmpty)
+          .map(user => user.usercode -> user.name.full.get)
+          .toMap,
+        students = userLookup
+          .getUsers(students.map(_.studentId))
+          .getOrElse(Map.empty)
+          .map {
+            case (universityId, user) => universityId -> user.name.full.getOrElse(universityId.string)
+          }
+      ))
+
+    }
   }
-
-
 
 }
 
