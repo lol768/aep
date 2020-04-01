195749a5a0e6fae64364a2ac5abdbb6c7b8e1dae
Kai Lan <k.lan@warwick.ac.uk>
2020-04-01 19:34:18 +0100

    OE-45 tidy up

--- app/controllers/invigilation/InvigilatorAssessmentController.scala
+++ app/controllers/invigilation/InvigilatorAssessmentController.scala
@@ -3,10 +3,13 @@ package controllers.invigilation
 import java.util.UUID
 
 import controllers.BaseController
+import domain.tabula
 import javax.inject.Inject
 import play.api.mvc.{Action, AnyContent}
 import services.refiners.ActionRefiners
+import services.tabula.TabulaDepartmentService
 import services.{AssessmentService, ReportingService, SecurityService, StudentAssessmentService}
+import warwick.core.helpers.ServiceResults
 import warwick.sso.{Name, UserLookupService, Usercode}
 
 import scala.concurrent.ExecutionContext
@@ -18,6 +21,7 @@ class InvigilatorAssessmentController @Inject()(
   reportingService: ReportingService,
   userLookup: UserLookupService,
   studentAssessmentService: StudentAssessmentService,
+  tabulaDepartmentService: TabulaDepartmentService,
 )(implicit ec: ExecutionContext) extends BaseController {
 
   import security._
@@ -25,29 +29,35 @@ class InvigilatorAssessmentController @Inject()(
   def view(assessmentId: UUID): Action[AnyContent] = InvigilatorAsseessmentAction(assessmentId).async { implicit req =>
 
     val assessment = req.assessment
-    // inflate dept code
 
-    studentAssessmentService.byAssessmentId(assessmentId).successMap { students =>
-      Ok(views.html.invigilation.assessment(
-        assessment = assessment,
-        userNames = assessment.invigilators
-          .map(userLookup.getUser)
-          .flatMap(_.fold(
-            _ => None,
-            user => Some(user)
-          ))
-          .filter(_.isFound)
-          .filter(_.name.full.nonEmpty)
-          .map(user => user.usercode -> user.name.full.get)
-          .toMap,
-        students = userLookup
-          .getUsers(students.map(_.studentId))
-          .getOrElse(Map.empty)
-          .map {
-            case (_, user) => user.usercode -> user.name.full.getOrElse(user.usercode.string)
-          }
-      ))
-
+    ServiceResults.zip(
+      tabulaDepartmentService.getDepartments,
+      studentAssessmentService.byAssessmentId(assessmentId),
+    ).successMap {
+      case (departments, students) =>
+        Ok(views.html.invigilation.assessment(
+          assessment = assessment,
+          userNames = assessment.invigilators
+            .map(userLookup.getUser)
+            .flatMap(_.fold(
+              _ => None,
+              user => Some(user)
+            ))
+            .filter(_.isFound)
+            .filter(_.name.full.nonEmpty)
+            .map(user => user.usercode -> user.name.full.get)
+            .toMap,
+          students = userLookup
+            .getUsers(students.map(_.studentId))
+            .getOrElse(Map.empty)
+            .map {
+              case (_, user) => user.usercode -> user.name.full.getOrElse(user.usercode.string)
+            },
+          department = departments.find { dept =>
+            dept.code == assessment.departmentCode.string ||
+              dept.parentDepartment.exists(_.code == assessment.departmentCode.string)
+          },
+        ))
     }
   }
 
