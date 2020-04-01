fedebf89a650f228ffb94dcf3b1cf8145ded0970
Adam Williams <A.Williams.9@warwick.ac.uk>
2020-04-01 15:53:59 +0100

    OE-131 Fix integration test assumption

--- it/test/scala/pages/AssessmentsListPage.scala
+++ it/test/scala/pages/AssessmentsListPage.scala
@@ -10,9 +10,12 @@ case class AssessmentInfo(title: String, viewLink: WebElement)
 class AssessmentsListPage()(implicit driver: WebDriver, server: ServerInfo) extends AbstractPage("Assessments List", driver, server) {
   override val path: String = "/assessments"
 
-  def assessmentsList: Seq[AssessmentInfo] = driver.findElements(By.className("assessment-information-panel")).asScala.map { div =>
-    val title = div.findElements(By.tagName("h4")).asScala.head.getText
-    val viewLink = div.findElements(By.tagName("a")).asScala.head
-    AssessmentInfo(title, viewLink)
-  }.toSeq
+  def assessmentsList: Seq[AssessmentInfo] = driver.findElements(By.className("assessment-information-panel")).asScala.filter { div =>
+    div.findElements(By.tagName("a")).asScala.nonEmpty
+  }.map { div =>
+      val title = div.findElements(By.tagName("h4")).asScala.head.getText
+      val viewLink = div.findElements(By.tagName("a")).asScala.head
+      AssessmentInfo(title, viewLink)
+    }.toSeq
+
 }
