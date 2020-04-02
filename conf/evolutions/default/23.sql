37207f89fada7017c3ac0331c47041efe1d672dc
Nick Kaijaks <Nick.Kaijaks@warwick.ac.uk>
2020-04-02 16:48:40 +0100

    OE-139b Fix the bad

--- conf/evolutions/default/23.sql
+++ conf/evolutions/default/23.sql
@@ -1,8 +1,21 @@
 # --- !Ups
-alter table uploaded_file add column upload_started timestamp(3) not null;
-alter table uploaded_file_version add column upload_started timestamp(3) not null;
+alter table uploaded_file
+    add column upload_started_utc timestamp(3);
+update uploaded_file
+    set upload_started_utc = created_utc;
+alter table uploaded_file
+    alter column upload_started_utc set not null;
 
+alter table uploaded_file_version
+    add column upload_started_utc timestamp(3);
+update uploaded_file_version
+    set upload_started_utc = created_utc;
+alter table uploaded_file_version
+    alter column upload_started_utc set not null;
 
 # --- !Downs
-alter table uploaded_file drop column upload_started;
-alter table uploaded_file_version drop column upload_started;
+alter table uploaded_file
+    drop column upload_started_utc;
+
+alter table uploaded_file_version
+    drop column upload_started_utc;
