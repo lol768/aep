a769b305fd0017f56333e4cfac84284836993aae
Nick Howes <n.howes@warwick.ac.uk>
2020-04-06 13:37:52 +0100

    OE-195 Update fetched content properly.

--- app/assets/js/studentAssessmentInfo.js
+++ app/assets/js/studentAssessmentInfo.js
@@ -3,8 +3,9 @@ const INTERVAL_MS = 30 * 1000;
 const el = document.querySelectorAll('.studentAssessmentInfo')[0];
 const refreshTable = () => setTimeout(() => {
   fetch(`/ajax/reporting/${el.getAttribute('data-id')}/${el.getAttribute('data-route')}`)
-    .then((response) => {
-      el.innerHtml = response.text();
+    .then((response) => response.text())
+    .then((responseText) => {
+      el.innerHTML = responseText;
     });
   refreshTable();
 }, INTERVAL_MS);
