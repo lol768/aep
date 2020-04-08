f2034f1df67e05888473790ff13f38395b9bf94d
Mat Mannion <M.Mannion@warwick.ac.uk>
2020-04-08 18:27:42 +0100

    Mistakes were made

--- conf/evolutions/default/30.sql
+++ conf/evolutions/default/30.sql
@@ -1,7 +1,7 @@
 # --- !Ups
 
 update assessment
-    set duration = null, type = null, platform = null
+    set duration = null, type = null, platform = ''
     where tabula_assessment_id is not null and brief = '{"fileIds": []}' and exam_profile_code in ('EXMAY20', 'EXJUN20_V2');
 
 # --- !Downs
