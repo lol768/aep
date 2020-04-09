18c6b37a98eb7db1c34f09218aae8ca96604dd6d
Kai Lan <k.lan@warwick.ac.uk>
2020-04-09 17:14:03 +0100

    OE-271 tidy up

--- app/controllers/api/ErrorsController.scala
+++ app/controllers/api/ErrorsController.scala
@@ -4,9 +4,10 @@ import controllers.BaseController
 import javax.inject.Singleton
 import net.logstash.logback.argument.StructuredArguments
 import org.slf4j.{Logger, LoggerFactory}
-import play.api.libs.json.{JsValue, Json}
+import play.api.libs.json.JsValue
 
 import scala.jdk.CollectionConverters._
+import scala.util.Try
 
 @Singleton
 class ErrorsController extends BaseController {
@@ -18,13 +19,13 @@ class ErrorsController extends BaseController {
       val message = error.get("message").flatMap(_.asOpt[String]).getOrElse("-")
       val entries = StructuredArguments.entries {
         Seq(
-          error.get("column").map("column" -> _.as[Int]),
-          error.get("line").map("line" -> _.as[Int]),
-          error.get("source").map("source" -> Json.prettyPrint(_)),
-          error.get("pageUrl").map("page_url" -> Json.prettyPrint(_)),
-          error.get("stack").map("stack_trace" -> Json.prettyPrint(_)),
+          error.get("column").flatMap(_.asOpt[String]).flatMap(s => Try(s.toInt).toOption).map("column" -> _),
+          error.get("line").flatMap(_.asOpt[String]).flatMap(s => Try(s.toInt).toOption).map("line" -> _),
+          error.get("source").flatMap(_.asOpt[String]).map("source" -> _),
+          error.get("pageUrl").flatMap(_.asOpt[String]).map("page_url" -> _),
+          error.get("stack").flatMap(_.asOpt[String]).map("stack_trace" -> _),
           Option(request.remoteAddress).map("source_ip" -> _),
-          request.headers("User-Agent").map(ua => "request_headers" -> Map("user-agent" -> ua)),
+          request.headers.get("User-Agent").map(ua => "request_headers" -> Map("user-agent" -> ua.toString)),
           request.user.map(_.usercode.string).map("username" -> _),
         ).flatten.toMap.asJava
       }
