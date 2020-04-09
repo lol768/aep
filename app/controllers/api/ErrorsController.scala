3aeb5a4bc3926501ee63e5e8a87d11f8751cda2e
Sam <s.boulby@warwick.ac.uk>
2020-04-09 11:11:24 +0100

    OE-271 Converted JSValues to string using Json.prettyPrint

--- app/controllers/api/ErrorsController.scala
+++ app/controllers/api/ErrorsController.scala
@@ -4,7 +4,7 @@ import controllers.BaseController
 import javax.inject.Singleton
 import net.logstash.logback.argument.StructuredArguments
 import org.slf4j.{Logger, LoggerFactory}
-import play.api.libs.json.JsValue
+import play.api.libs.json.{JsValue, Json}
 
 import scala.jdk.CollectionConverters._
 
@@ -18,11 +18,11 @@ class ErrorsController extends BaseController {
       val message = error.get("message").flatMap(_.asOpt[String]).getOrElse("-")
       val entries = StructuredArguments.entries {
         Seq(
-          error.get("column").map("column" -> _),
-          error.get("line").map("line" -> _),
-          error.get("source").map("source" -> _),
-          error.get("pageUrl").map("page_url" -> _),
-          error.get("stack").map("stack_trace" -> _),
+          error.get("column").map("column" -> Json.prettyPrint(_)),
+          error.get("line").map("line" -> Json.prettyPrint(_)),
+          error.get("source").map("source" -> Json.prettyPrint(_)),
+          error.get("pageUrl").map("page_url" -> Json.prettyPrint(_)),
+          error.get("stack").map("stack_trace" -> Json.prettyPrint(_)),
           Option(request.remoteAddress).map("source_ip" -> _),
           request.headers("User-Agent").map(ua => "request_headers" -> Map("user-agent" -> ua)),
           request.user.map(_.usercode.string).map("username" -> _),
