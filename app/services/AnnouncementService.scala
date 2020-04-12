45b57349e897aa77195cb541b9d17ae6d523220a
Mat Mannion <M.Mannion@warwick.ac.uk>
2020-04-12 20:54:43 +0100

    OE-267 Send announcements as My Warwick alerts

--- app/services/AnnouncementService.scala
+++ app/services/AnnouncementService.scala
@@ -10,21 +10,26 @@ import domain.Announcement
 import domain.dao.AnnouncementsTables.StoredAnnouncement
 import domain.dao.{AnnouncementDao, DaoRunner}
 import javax.inject.{Inject, Singleton}
+import org.quartz.Scheduler
+import play.api.mvc.RequestHeader
+import uk.ac.warwick.util.mywarwick.MyWarwickService
+import uk.ac.warwick.util.mywarwick.model.request.Activity
 import warwick.core.helpers.ServiceResults
+import warwick.core.helpers.ServiceResults.Implicits._
 import warwick.core.helpers.ServiceResults.ServiceResult
 import warwick.core.system.AuditLogContext
 import warwick.core.timing.TimingContext
-import warwick.core.helpers.ServiceResults.Implicits._
-import warwick.sso.UniversityID
+import warwick.sso.UserLookupService
 
 import scala.concurrent.{ExecutionContext, Future}
+import scala.jdk.CollectionConverters._
 
 @ImplementedBy(classOf[AnnouncementServiceImpl])
 trait AnnouncementService {
   def list(implicit t: TimingContext): Future[ServiceResult[Seq[Announcement]]]
   def get(id: UUID)(implicit t: TimingContext): Future[ServiceResult[Announcement]]
   def getByAssessmentId(id: UUID)(implicit t: TimingContext): Future[ServiceResult[Seq[Announcement]]]
-  def save(announcement: Announcement)(implicit ac: AuditLogContext): Future[ServiceResult[Done]]
+  def save(announcement: Announcement)(implicit ac: AuditLogContext, request: RequestHeader): Future[ServiceResult[Done]]
   def delete(id: UUID)(implicit t: TimingContext): Future[ServiceResult[Done]]
 }
 
@@ -34,10 +39,14 @@ class AnnouncementServiceImpl @Inject()(
   daoRunner: DaoRunner,
   dao: AnnouncementDao,
   pubSubService: PubSubService,
+  assessmentService: AssessmentService,
   studentAssessmentService: StudentAssessmentService,
+  myWarwickService: MyWarwickService,
+  scheduler: Scheduler,
+  userLookupService: UserLookupService,
 )(implicit ec: ExecutionContext) extends AnnouncementService {
 
-  override def save(announcement: Announcement)(implicit ac: AuditLogContext): Future[ServiceResult[Done]] = {
+  override def save(announcement: Announcement)(implicit ac: AuditLogContext, request: RequestHeader): Future[ServiceResult[Done]] = {
     val stored = StoredAnnouncement(
       id = announcement.id,
       assessmentId = announcement.assessment,
@@ -45,10 +54,34 @@ class AnnouncementServiceImpl @Inject()(
       created = announcement.created,
       version = OffsetDateTime.now()
     )
+
     pubSubService.publish(
       topic = announcement.assessment.toString,
       AssessmentAnnouncement(announcement.text, announcement.created)
     )
+
+    // Intentionally fire-and-forget to send the announcement via My Warwick as well
+    ServiceResults.zip(
+      assessmentService.get(announcement.assessment),
+      studentAssessmentService.byAssessmentId(announcement.assessment),
+    ).successMapTo { case (assessment, students) =>
+      val universityIds = students.map(_.studentId)
+      userLookupService.getUsers(universityIds).toOption.foreach { users =>
+        val usercodes = users.values.map(_.usercode.string).toSet
+
+        myWarwickService.queueNotification(
+          new Activity(
+            usercodes.asJava,
+            s"${assessment.paperCode} ${assessment.title}: Announcement",
+            controllers.routes.AssessmentController.view(assessment.id).absoluteURL(),
+            announcement.text,
+            "assessment-announcement"
+          ),
+          scheduler
+        )
+      }
+    }
+
     daoRunner.run(dao.insert(stored)).map(_ => ServiceResults.success(Done))
   }
 
