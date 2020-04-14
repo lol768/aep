d87647f9d448162fc7730ff35110a7ec72360df5
Sam <s.boulby@warwick.ac.uk>
2020-04-14 09:36:26 +0100

    OE-249 Colin

--- test/assets/js/assessment-announcements-test.js
+++ test/assets/js/assessment-announcements-test.js
@@ -12,15 +12,13 @@ describe('Assessment Announcements', () => {
 
   it('should nicely format a simple message provided by the websocket', () => {
     const messageData = {
-      message: "This is the captain of your ship \n calling...",
+      message: "This is the captain of your ship, \n Colin...",
       timestamp: 1586850896285
     };
 
-    const expectedFormat = '<div class="media-left"><i aria-hidden="true" class="fad fa-bullhorn"></i></div><div class="media-body">This is the captain of your ship <br> calling...<div class="query-time">1586850896285</div></div>'
+    const expectedFormat = '<div class="alert alert-info media"><div class="media-left"><i aria-hidden="true" class="fad fa-bullhorn"></i></div><div class="media-body">This is the captain of your ship, <br> Colin...<div class="query-time">1586850896285</div></div></div>'
     const formattedMessage = formatAnnouncement(messageData);
-    expect(formattedMessage.tagName).equals('DIV');
-    expect(formattedMessage.className).equals('alert alert-info media');
-    expect(formattedMessage.innerHTML).equals(expectedFormat);
+    expect(formattedMessage.outerHTML).equals(expectedFormat);
   });
 
 });
