b5ebe1fc0fffb1834caa744cd75fb6449b227500
Alec Cursley <alec@cursley.net>
2020-04-14 16:24:05 +0100

    OE-304 Update document.title with number of queries
    Looks for a data-document-title-prefix attribute and prepends the value
    to the original document title.

--- app/assets/js/studentAssessmentInfo.js
+++ app/assets/js/studentAssessmentInfo.js
@@ -1,13 +1,29 @@
 const INTERVAL_MS = 30 * 1000;
 
+const originalDocumentTitle = document.title;
+function updateDocumentTitle() {
+  let prefix = '';
+  const titlePrefixElement = document.querySelector('[data-document-title-prefix]');
+  if (titlePrefixElement) {
+    prefix = titlePrefixElement.getAttribute('data-document-title-prefix');
+  }
+  if (prefix.trim().length !== 0) {
+    document.title = `${prefix.trim()} ${originalDocumentTitle}`;
+  } else {
+    document.title = originalDocumentTitle;
+  }
+}
+
 const el = document.querySelectorAll('.studentAssessmentInfo')[0];
 const refreshTable = () => setTimeout(() => {
   fetch(`/ajax/reporting/${el.getAttribute('data-id')}/${el.getAttribute('data-route')}`)
     .then((response) => response.text())
     .then((responseText) => {
       el.innerHTML = responseText;
+      updateDocumentTitle();
     });
   refreshTable();
 }, INTERVAL_MS);
 
 refreshTable();
+updateDocumentTitle();
