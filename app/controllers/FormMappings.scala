22bdbf916edc2b8205dab64dd3c1c78c60717a85
Matt Jones <matthew.jones@warwick.ac.uk>
2020-04-15 14:59:41 +0100

    OE-307 - Allow unused mock assessments to be deleted

--- app/controllers/FormMappings.scala
+++ app/controllers/FormMappings.scala
@@ -1,7 +1,9 @@
 package controllers
 
 import java.time.OffsetDateTime
-import play.api.data.{Forms, Mapping}
+
+import play.api.data.Forms.{checked, mapping}
+import play.api.data.{Form, Forms, Mapping}
 import warwick.core.helpers.JavaTime
 
 object FormMappings {
@@ -9,4 +11,8 @@ object FormMappings {
   val Html5LocalDateTimePattern = "yyyy-MM-dd'T'HH:mm"
   val offsetDateTime: Mapping[OffsetDateTime] =
     Forms.localDateTime(Html5LocalDateTimePattern).transform[OffsetDateTime](_.atZone(JavaTime.timeZone).toOffsetDateTime, _.toLocalDateTime)
+
+  val confirmForm: Form[Boolean] = Form(mapping(
+    "confirm" -> checked("You must check this box")
+  )(b => b)(b => Some(b)))
 }
