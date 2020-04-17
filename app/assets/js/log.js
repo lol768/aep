46a1973e845f1b7410d4a714d70027ad43f6a0c9
Mat Mannion <M.Mannion@warwick.ac.uk>
2020-04-17 09:57:23 +0100

    OE-354 Fix 'Invalid calling object' in Edge 16 due to a problem with our console wrapper

--- app/assets/js/log.js
+++ app/assets/js/log.js
@@ -3,9 +3,9 @@
 const noop = () => {};
 
 /* eslint-disable no-console */
-const info = (window.console && console.info) ? console.log : noop;
-const error = (window.console && console.error) ? console.error : info;
-const warn = (window.console && console.warn) ? console.error : info;
+const info = (window.console && console.log) ? console.log.bind(console) : noop;
+const error = (window.console && console.error) ? console.error.bind(console) : info;
+const warn = (window.console && console.warn) ? console.warn.bind(console) : info;
 
 export { error, warn, info };
 export default info;
