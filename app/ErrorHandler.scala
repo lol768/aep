47d318ca4c5bbee49c2e91c2ada727741d8e6453
Sam <s.boulby@warwick.ac.uk>
2020-04-21 10:54:59 +0100

    OE-301 Show errors to sysadmins regardless of app environment mode

--- app/ErrorHandler.scala
+++ app/ErrorHandler.scala
@@ -5,9 +5,10 @@ import play.api.{Configuration, Environment, Mode}
 import play.api.http.{DefaultHttpErrorHandler, Status}
 import play.api.libs.json.Json
 import play.api.mvc._
-import system.ImplicitRequestContext
+import system.{ImplicitRequestContext, Roles}
 import warwick.core.Logging
 import warwick.fileuploads.UploadedFileControllerHelper.UploadedFileConfiguration
+import warwick.sso.{GroupService, RoleService}
 
 import scala.concurrent.Future
 
@@ -18,9 +19,21 @@ import scala.concurrent.Future
 class ErrorHandler @Inject()(
   environment: Environment,
   config: Configuration,
+  groupService: GroupService,
+  roleService: RoleService,
 ) extends DefaultHttpErrorHandler with Results with Status with Logging with Rendering with AcceptExtractors with ImplicitRequestContext {
 
   private[this] val uploadedFileConfiguration = UploadedFileConfiguration.fromConfiguration(config)
+
+  private def isSysAdmin(request: RequestHeader): Boolean = {
+    request.actualUser.exists { user =>
+      val role = roleService.getRole(Roles.Sysadmin)
+      groupService.isUserInGroup(user.usercode, role.groupName).fold (
+        _ => false,
+        (result: Boolean) => result
+      )
+    }
+  }
 
   override def onClientError(request: RequestHeader, statusCode: Int, message: String): Future[Result] = {
     implicit val context: RequestContext = requestContext(request)
@@ -62,13 +75,13 @@ class ErrorHandler @Inject()(
           InternalServerError(Json.toJson(JsonClientError(
             status = "internal_server_error",
             errors = {
-              if (environment.mode == Mode.Dev || environment.mode == Mode.Test)
+              if (environment.mode == Mode.Dev || environment.mode == Mode.Test || isSysAdmin(requestHeader))
                 Seq(exception.getMessage)
               else
                 Seq("Sorry, there's been a problem and we weren't able to complete your request")
             }
           )))
-        case _ => InternalServerError(views.html.errors.serverError(exception, environment.mode))
+        case _ => InternalServerError(views.html.errors.serverError(exception, environment.mode, isSysAdmin(requestHeader)))
       }(request)
     )
   }
