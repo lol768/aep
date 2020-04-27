ab04dea225d1e33c2c6d5d898aafe64c55a9813c
Mat Mannion <M.Mannion@warwick.ac.uk>
2020-04-27 09:12:46 +0100

    OE-444 Allow submission CSV file to be downloaded directly for non-AEP assessments

--- app/controllers/admin/AdminSubmissionsDownloadController.scala
+++ app/controllers/admin/AdminSubmissionsDownloadController.scala
@@ -6,7 +6,7 @@ import controllers.AssessmentSubmissionsDownloadController
 import javax.inject.{Inject, Singleton}
 import org.quartz.Scheduler
 import play.api.mvc.{Action, AnyContent}
-import services.{SecurityService, UploadedFileService}
+import services.{SecurityService, StudentAssessmentService, UploadedFileService}
 import warwick.fileuploads.UploadedFileControllerHelper
 
 import scala.concurrent.ExecutionContext
@@ -17,12 +17,19 @@ class AdminSubmissionsDownloadController @Inject()(
   scheduler: Scheduler,
   uploadedFileService: UploadedFileService,
   uploadedFileControllerHelper: UploadedFileControllerHelper,
+  studentAssessmentService: StudentAssessmentService,
 )(implicit ec: ExecutionContext) extends AssessmentSubmissionsDownloadController(scheduler, uploadedFileService, uploadedFileControllerHelper) {
 
   import security._
 
   def download(assessmentId: UUID): Action[AnyContent] = AssessmentDepartmentAdminAction(assessmentId).async { implicit request =>
     doDownload(request.assessment)
+  }
+
+  def submissionsCSV(assessmentId: UUID): Action[AnyContent] = AssessmentDepartmentAdminAction(assessmentId).async { implicit request =>
+    studentAssessmentService.sittingsByAssessmentId(assessmentId).successMap { sittings =>
+      doSubmissionsCSV(request.assessment, sittings)
+    }
   }
 
 }
