689a40b598a03df71494fd20e4b7ae574e9197cf
Sam <s.boulby@warwick.ac.uk>
2020-11-11 07:19:25 +0000

    OE-492 Added explainer comment

--- app/domain/Declarations.scala
+++ app/domain/Declarations.scala
@@ -7,7 +7,7 @@ import system.Features
 case class Declarations(
   studentAssessmentId: UUID,
   acceptsAuthorship: Boolean = false,
-  selfDeclaredRA: Option[Boolean] = None,
+  selfDeclaredRA: Option[Boolean] = None, // Will be None if RA is imported because the form is not shown. That way we have a record of whether the import was on or not when the declaration was signed.
   completedRA: Boolean = false
 ) {
   def acceptable(features: Features): Boolean =
